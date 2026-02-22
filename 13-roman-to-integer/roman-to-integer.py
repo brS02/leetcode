6174bf87cf8312e46fb07ddf0f4255f2562e6e7f
Brandon Smith <97079039+brS02@users.noreply.github.com>
2026-02-23 07:59:21 +0900

    Time: 3 ms (80.55%) | Memory: 19.3 MB (49.96%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        if len(s)==0 or len(s)>15:
+            return
+    
+        num = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+      
+        res = 0
+        i = 0
+        while i < len(s):
+            cur = num[s[i]]
+
+            if i+1 < len(s) and cur < num[s[i+1]]:
+                res += num[s[i+1]] - cur
+                i += 2
+
+            else:
+                res += cur
+                i += 1
+            
+        return res
