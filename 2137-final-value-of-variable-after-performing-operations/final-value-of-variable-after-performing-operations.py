4eb0003a8438ea908ff165793d184ce145ab5bde
Brandon Smith <97079039+brS02@users.noreply.github.com>
2026-03-10 14:04:14 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (84.84%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -2,12 +2,8 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         ans = 0
         for operation in operations:
-            if operation == "++X":
+            if operation == "++X" or operation == "X++":
                 ans += 1
-            if operation == "X++":
-                ans += 1
-            if operation == "X--": 
-                ans -= 1
-            if operation == "--X":
+            elif operation == "X--" or operation == "--X": 
                 ans -= 1
         return ans
