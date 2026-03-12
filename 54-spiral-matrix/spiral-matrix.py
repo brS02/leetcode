781e436515f831e4d9b5865a4f53619b83d13664
Brandon Smith <97079039+brS02@users.noreply.github.com>
2026-03-12 15:16:08 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (98.53%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -3,15 +3,19 @@ class Solution:
         ans = []
         
         while matrix:
+            # top
             ans += matrix.pop(0)
 
+            # right
             if matrix and matrix[0]:
                 for row in matrix:
                     ans.append(row.pop())
             
+            # bottom
             if matrix:
                 ans += matrix.pop()[::-1]
             
+            # left
             if matrix and matrix[0]:
                 for row in matrix[::-1]:
                     ans.append(row.pop(0))
