4257c4eb654c3d10843952ab149a4b3482273ce2
Brandon Smith <97079039+brS02@users.noreply.github.com>
2026-06-04 15:35:08 -0600

    Time: 7 ms (28.89%) | Memory: 20.5 MB (75.39%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,21 +1,20 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        ans = []
 
         if numbers:
             x = 0 # lower bound
             y = len(numbers)-1 #upper bound
 
             while x < y:
-                if (numbers[x] + numbers[y]) == target: # if lower and upper index = target, return
-                    ans.append(x + 1)
-                    ans.append(y + 1)
-                    break
+                total = numbers[x] + numbers[y]
 
-                elif (numbers[x] + numbers[y]) > target: # if sum too big, decrement upper limit
+                if total == target: # if lower and upper index = target, return
+                    return [x+1, y+1]
+
+                elif total > target: # if sum too big, decrement upper limit
                     y -= 1
                 
                 else: # if sum too small, incremement lower bound
                     x += 1
 
-        return ans
+        return []
